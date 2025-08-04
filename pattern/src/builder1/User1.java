c57231b4e161d13bb533b3b46ac207ea4aa15319
jhyeo <juhyeon292@knou.ac.kr>
2025-08-04 18:17:45 +0900

    builder

--- pattern/src/builder1/User1.java
+++ pattern/src/builder1/User1.java
@@ -6,5 +6,26 @@ public class User1 {
 	private String email;
 	private int age;
 	private String phone;
+	
+	private User1() {
+		
+	}
+	
+	public class builder {
+		String name;
+		String email;
+		int age;
+		String phone;
+		
+		public builder(String name, String email) {
+			this.name = name;
+			this.email = email;
+		}
+		
+		public builder age(int age) {
+			this.age = age;
+			return this;
+		}
+	}
 
 }
