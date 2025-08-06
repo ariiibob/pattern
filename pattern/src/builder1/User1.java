c1e4399df78b57d039cbb74736aa994ef7696fcf
jhyeo <juhyeon292@knou.ac.kr>
2025-08-06 16:37:52 +0900

    builder

--- pattern/src/builder1/User1.java
+++ pattern/src/builder1/User1.java
@@ -2,20 +2,25 @@ package builder1;
 
 public class User1 {
 	
-	private String name;
-	private String email;
-	private int age;
-	private String phone;
+	private final String name;
+	private final String email;
+	private final int age;
+	private final String phone;
 	
-	private User1() {
-		
+	private User1(builder builder) {
+        this.name = builder.name;
+        this.email = builder.email;
+        this.age = builder.age;
+        this.phone = builder.phone;
 	}
 	
-	public class builder {
-		String name;
-		String email;
-		int age;
-		String phone;
+	public static class builder {
+		
+		private final String name;
+		private final String email;
+		
+		private int age = 0;
+		private String phone = "";
 		
 		public builder(String name, String email) {
 			this.name = name;
@@ -26,6 +31,15 @@ public class User1 {
 			this.age = age;
 			return this;
 		}
+		
+		public builder phone(String phone) {
+			this.phone = phone;
+			return this;
+		}
+		
+        public User1 build() {
+            return new User1(this);
+        }
 	}
 
 }
